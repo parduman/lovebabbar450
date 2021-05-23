0ccd66c935ceefeaaa44e6ee7c50b88ee4782814
parduman <pardumangarg7@gmail.com>
2021-05-23 18:59:34 +0530

    tree questions

--- tree/leftViewOfTree.py
+++ tree/leftViewOfTree.py
@@ -5,16 +5,13 @@ class Node:
         self.left = None
         self.right = None
 
-maxheight=-1
-def leftViewOfTree(node, height=0):
-    global maxheight
+def leftViewOfTree(node, height, maxheight):
     if not node: return
-    if(height>maxheight):
+    if(height>maxheight[0]):
         print(node.data)
-        maxheight += 1
-    height += 1
-    if node.left: leftViewOfTree(node.left, height)
-    if node.right: leftViewOfTree(node.right, height)
+        maxheight[0] += 1
+    if node.left: leftViewOfTree(node.left, height+1, maxheight)
+    if node.right: leftViewOfTree(node.right, height+1, maxheight)
     
 
 
@@ -27,4 +24,35 @@ root.right.right = Node(6)
 root.right.left.left = Node(7)
 root.right.left.right = Node(8)
 
-leftViewOfTree(root)
+maxheight = [-1]
+leftViewOfTree(root,0,maxheight)
+
+print(maxheight)
+
+
+# geekForGeeksSolution
+
+
+# maxheight=-1
+# arr = []
+# def LeftView(node):
+#     global arr,maxheight
+#     LeftViewTree(node,0)
+#     arrToReturn = arr
+#     arr = []
+#     maxheight = -1
+#     return arrToReturn
+#     # code here
+
+# def LeftViewTree(node, height = 0):
+#     global maxheight, arr
+#     if not node: return
+#     if(height>maxheight):
+#         arr.append(node.data)
+#         maxheight += 1
+#     height += 1
+#     if node.left: LeftViewTree(node.left, height)
+#     if node.right: LeftViewTree(node.right, height)
+#     return arr
+
+
